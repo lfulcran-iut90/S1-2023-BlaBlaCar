2923bafb45114ae340f6f933e1d0a1e496cbff6c
jb_frhl <146169903+jbfroehl-iut90@users.noreply.github.com>
2023-10-19 19:11:51 +0200

    finex je fais mon js

--- Site/src/scripts/graph_eco.js
+++ Site/src/scripts/graph_eco.js
@@ -1,26 +1,19 @@
-new Chart(
-    document.getElementById('myChart'),
-    {
-      type: 'bar',
-      options: {
-        animation: false,
-        plugins: {
-          legend: {
-            display: false
-          },
-          tooltip: {
-            enabled: false
-          }
-        }
-      },
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
-          }
-        ]
-      }
-    }
-  );
+const blaBlaCanvas = document.getElementById('myChart');
+
+const blaBlaChart = new Chart(blaBlaCanvas, {
+  // Graphe sur un chiffre d'affaire en augmentation
+  type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  data: {
+    labels: ['2018', '2019', '2020', '2021', '2022', 'Juin'],
+    datasets: [{
+      label: 'Chiffre d\'affaire',
+      data: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160,170,180,190,200],
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderWidth: 1,
+      borderColor: '#777',
+      hoverBorderWidth: 3,
+      hoverBorderColor: '#000'
+    }]
+  },
+
+})
--- Site/src/scripts/script.js
+++ Site/src/scripts/script.js
@@ -228,26 +228,3 @@ if (pageTitle.text === "VroumVroumGo - BlaBlaCar") {
     window.addEventListener("click", boxModelFun);
     boxModelArrow.addEventListener("click", boxModelFun);
 }
-
-// Graphiques page eco
-
-const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-});
