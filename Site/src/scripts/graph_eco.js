31e7f91ce7a5ae881a62cbf74fc36c10e737b4f1
SirThirrygolooo <146169903+jbfroehl-iut90@users.noreply.github.com>
2023-10-19 10:28:02 +0200

    :)

---
+++ Site/src/scripts/graph_eco.js
@@ -0,0 +1,26 @@
+new Chart(
+    document.getElementById('myChart'),
+    {
+      type: 'bar',
+      options: {
+        animation: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            enabled: false
+          }
+        }
+      },
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
--- Site/src/scripts/script.js
+++ Site/src/scripts/script.js
@@ -229,5 +229,25 @@ if (pageTitle.text === "VroumVroumGo - BlaBlaCar") {
     boxModelArrow.addEventListener("click", boxModelFun);
 }
 
+// Graphiques page eco
 
-// Tests
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
